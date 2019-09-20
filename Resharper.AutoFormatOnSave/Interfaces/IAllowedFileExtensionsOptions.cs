5efbd19dcf38757c07f3a2ffb447fb24a15ac49c
Simon Gockner <simon.gockner@gmail.com>
2019-09-20 15:27:01 +0200

    - add interfaces for option pages

---
+++ Resharper.AutoFormatOnSave/Interfaces/IAllowedFileExtensionsOptions.cs
@@ -0,0 +1,49 @@
+﻿// Author: Gockner, Simon
+// Created: 2019-09-20
+// Copyright(c) 2019 SimonG. All Rights Reserved.
+
+namespace Resharper.AutoFormatOnSave.Interfaces
+{
+    public interface IAllowedFileExtensionsOptions
+    {
+        /// <summary>
+        /// Is .cs file extension allowed
+        /// </summary>
+        bool IsCsAllowed { get; set; }
+
+        /// <summary>
+        /// Is .xaml file extension allowed
+        /// </summary>
+        bool IsXamlAllowed { get; set; }
+
+        /// <summary>
+        /// Is .vb file extension allowed
+        /// </summary>
+        bool IsVbAllowed { get; set; }
+
+        /// <summary>
+        /// Is .js file extension allowed
+        /// </summary>
+        bool IsJsAllowed { get; set; }
+
+        /// <summary>
+        /// Is .ts file extension allowed
+        /// </summary>
+        bool IsTsAllowed { get; set; }
+
+        /// <summary>
+        /// Is .css file extension allowed
+        /// </summary>
+        bool IsCssAllowed { get; set; }
+
+        /// <summary>
+        /// Is .html file extension allowed
+        /// </summary>
+        bool IsHtmlAllowed { get; set; }
+
+        /// <summary>
+        /// Is .xml file extension allowed
+        /// </summary>
+        bool IsXmlAllowed { get; set; }
+    }
+}
---
+++ Resharper.AutoFormatOnSave/Interfaces/ILoggingOptions.cs
@@ -0,0 +1,14 @@
+﻿// Author: Gockner, Simon
+// Created: 2019-09-20
+// Copyright(c) 2019 SimonG. All Rights Reserved.
+
+namespace Resharper.AutoFormatOnSave.Interfaces
+{
+    public interface ILoggingOptions
+    {
+        /// <summary>
+        /// The <see cref="AutoFormatOnSave.LogLevel"/> of the extension
+        /// </summary>
+        LogLevel LogLevel { get; set; }
+    }
+}
