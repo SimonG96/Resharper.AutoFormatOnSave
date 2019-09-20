17c3d57227e7b37075d4cdbd67545169d07e9d19
Simon Gockner <simon.gockner@gmail.com>
2019-09-20 11:39:05 +0200

    - introduce logging

---
+++ Resharper.AutoFormatOnSave/ILog.cs
@@ -0,0 +1,14 @@
+﻿// Author: Gockner, Simon
+// Created: 2019-09-20
+// Copyright(c) 2019 SimonG. All Rights Reserved.
+
+using System;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace Resharper.AutoFormatOnSave
+{
+    public interface ILog : IDisposable
+    {
+        void InitializeLog(IVsOutputWindowPane outputPane);
+    }
+}
---
+++ Resharper.AutoFormatOnSave/Log.cs
@@ -0,0 +1,32 @@
+﻿// Author: Gockner, Simon
+// Created: 2019-09-20
+// Copyright(c) 2019 SimonG. All Rights Reserved.
+
+using System;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace Resharper.AutoFormatOnSave
+{
+    public class Log : ILog
+    {
+        private static IVsOutputWindowPane _outputPane;
+
+        public void InitializeLog(IVsOutputWindowPane outputPane)
+        {
+            _outputPane = outputPane;
+        }
+
+        public static void WriteLine(string message)
+        {
+            _outputPane.OutputString($"{message}\n");
+            _outputPane.Activate();
+        }
+
+        public void Dispose()
+        {
+            _outputPane.Hide();
+            _outputPane.Clear();
+            _outputPane = null;
+        }
+    }
+}
