479272117a02ae98ecdce8e8fbedb521b85eaf92
Simon Gockner <simon.gockner@gmail.com>
2019-09-20 11:28:36 +0200

    - add guids to their own file

---
+++ Resharper.AutoFormatOnSave/Guids.cs
@@ -0,0 +1,21 @@
+﻿// Author: Gockner, Simon
+// Created: 2019-09-20
+// Copyright(c) 2019 SimonG. All Rights Reserved.
+
+using System;
+
+namespace Resharper.AutoFormatOnSave
+{
+    public static class Guids
+    {
+        /// <summary>
+        /// AutoFormatOnSavePackage GUID string
+        /// </summary>
+        public const string PACKAGE_GUID_STRING = "9a82f1dc-d8e7-4ab9-82e9-0d2691de3bbd";
+
+        /// <summary>
+        /// Output pane GUID
+        /// </summary>
+        public static readonly Guid OUTPUT_PANE_GUID = new Guid("F540D10F-7426-4E03-9ACE-C229E85B42DE");
+    }
+}
