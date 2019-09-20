f56978ec5a5fa3f40d3df893b2b0ee62f0336280
Simon Gockner <simon.gockner@gmail.com>
2019-09-20 15:26:28 +0200

    - introduce logLevels

---
+++ Resharper.AutoFormatOnSave/LogLevel.cs
@@ -0,0 +1,14 @@
+﻿// Author: Gockner, Simon
+// Created: 2019-09-20
+// Copyright(c) 2019 SimonG. All Rights Reserved.
+
+namespace Resharper.AutoFormatOnSave
+{
+    public enum LogLevel
+    {
+        None,
+        Default,
+        Advanced,
+        Debug
+    }
+}
